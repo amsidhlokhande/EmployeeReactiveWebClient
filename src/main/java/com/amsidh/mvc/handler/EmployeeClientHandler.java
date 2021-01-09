9052fb4b426544de645a2ea5ba428c633ae50b57
amsidhlokhande <amsidhlokhande@gmail.com>
2021-01-09 17:21:17 +0530

    Updated the handler with exchange and retrieve webclient api's

--- src/main/java/com/amsidh/mvc/handler/EmployeeClientHandler.java
+++ src/main/java/com/amsidh/mvc/handler/EmployeeClientHandler.java
@@ -1,5 +1,7 @@
 package com.amsidh.mvc.handler;
 
+import java.time.Duration;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -17,12 +19,22 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class EmployeeClientHandler {
 
+	private static final String EMPLOYEES_BASE_URI = "/employees";
+	private static final String EMPLOYEES_BASE_URI_WITH_ID = "/employees/{id}";
 	WebClient webClient = WebClient.create("http://localhost:8989");
 
 	public Mono<ServerResponse> getEmployees(ServerRequest serverRequest) {
 		log.info("EmployeeClientHandler getEmployees called");
-		Flux<Employee> employeeFlux = webClient.get().uri("/employees").accept(MediaType.APPLICATION_JSON).retrieve()
-				.bodyToFlux(Employee.class);
+		Flux<Employee> employeeFlux = webClient.get().uri(EMPLOYEES_BASE_URI).accept(MediaType.APPLICATION_JSON).retrieve()
+				.bodyToFlux(Employee.class).delayElements(Duration.ofSeconds(1)).log();
+		return ServerResponse.ok().body(employeeFlux, Employee.class);
+
+	}
+
+	public Mono<ServerResponse> getEmployeesUsingExchange(ServerRequest serverRequest) {
+		log.info("EmployeeClientHandler getEmployeesUsingExchange called");
+		Flux<Employee> employeeFlux = webClient.get().uri(EMPLOYEES_BASE_URI).accept(MediaType.APPLICATION_JSON)
+				.exchangeToFlux(clientResponse -> clientResponse.bodyToFlux(Employee.class));
 		return ServerResponse.ok().body(employeeFlux, Employee.class);
 
 	}
@@ -30,32 +42,30 @@ public class EmployeeClientHandler {
 	public Mono<ServerResponse> getEmployeeById(ServerRequest serverRequest) {
 		log.info("EmployeeClientHandler getEmployees called");
 		Mono<Employee> employeeMono = webClient.get()
-				.uri(String.format("/employees/%s", Integer.parseInt(serverRequest.pathVariable("id"))))
-				.accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(Employee.class);
+				.uri(EMPLOYEES_BASE_URI_WITH_ID, Integer.parseInt(serverRequest.pathVariable("id")))
+				.accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(Employee.class).log();
 		return ServerResponse.ok().body(employeeMono, Employee.class);
 
 	}
 
 	public Mono<ServerResponse> saveEmployee(ServerRequest serverRequest) {
 		log.info("EmployeeClientHandler saveEmployee called");
-		Mono<Employee> employeeMono = webClient.post()
-				.uri("/employees")
-				.accept(MediaType.APPLICATION_JSON)
+		Mono<Employee> employeeMono = webClient.post().uri(EMPLOYEES_BASE_URI).accept(MediaType.APPLICATION_JSON)
 				.contentType(MediaType.APPLICATION_JSON)
-				.body(BodyInserters.fromProducer(serverRequest.bodyToMono(Employee.class), Employee.class)).retrieve()
-				.bodyToMono(Employee.class);
+				//.body(BodyInserters.fromProducer(serverRequest.bodyToMono(Employee.class), Employee.class)).retrieve() //This will also works
+				.body(serverRequest.bodyToMono(Employee.class), Employee.class).retrieve()
+				.bodyToMono(Employee.class).log();
 		return ServerResponse.ok().body(employeeMono, Employee.class);
 
 	}
 
-	
 	public Mono<ServerResponse> updateEmployee(ServerRequest serverRequest) {
 		log.info("EmployeeClientHandler updateEmployee called");
 		Mono<Employee> employeeMono = webClient.patch()
-				.uri(String.format("/employees/%s", Integer.parseInt(serverRequest.pathVariable("id"))))
+				.uri(EMPLOYEES_BASE_URI_WITH_ID, Integer.parseInt(serverRequest.pathVariable("id")))
 				.contentType(MediaType.APPLICATION_JSON)
 				.body(BodyInserters.fromProducer(serverRequest.bodyToMono(Employee.class), Employee.class)).retrieve()
-				.bodyToMono(Employee.class);
+				.bodyToMono(Employee.class).log();
 		return ServerResponse.ok().body(employeeMono, Employee.class);
 
 	}
@@ -63,8 +73,8 @@ public class EmployeeClientHandler {
 	public Mono<ServerResponse> deleteEmployee(ServerRequest serverRequest) {
 		log.info("EmployeeClientHandler deleteEmployee called");
 		Mono<Void> employeeMono = webClient.delete()
-				.uri(String.format("/employees/%s", Integer.parseInt(serverRequest.pathVariable("id")))).retrieve()
-				.bodyToMono(Void.class);
+				.uri(EMPLOYEES_BASE_URI_WITH_ID, Integer.parseInt(serverRequest.pathVariable("id"))).retrieve()
+				.bodyToMono(Void.class).log();
 		return ServerResponse.ok().body(employeeMono, Void.class);
 
 	}
--- src/main/java/com/amsidh/mvc/routes/EmpolyeeClientRoute.java
+++ src/main/java/com/amsidh/mvc/routes/EmpolyeeClientRoute.java
@@ -16,6 +16,7 @@ public class EmpolyeeClientRoute {
 	@Bean
 	public RouterFunction<ServerResponse> getEmployeeRoutes(EmployeeClientHandler employeeClientHandler) {
 		return RouterFunctions.route(RequestPredicates.GET("/webclient/employees"), employeeClientHandler::getEmployees)
+				.and(RouterFunctions.route(RequestPredicates.GET("/webclient/employees/flux"), employeeClientHandler::getEmployeesUsingExchange))
 				.and(RouterFunctions.route(RequestPredicates.GET("/webclient/employees/{id}"), employeeClientHandler::getEmployeeById))
 				.and(RouterFunctions.route(RequestPredicates.POST("/webclient/employees"), employeeClientHandler::saveEmployee))
 				.and(RouterFunctions.route(RequestPredicates.PATCH("/webclient/employees/{id}"), employeeClientHandler::updateEmployee))
